3ff85df3d227b728938129064f93724e9f8424ae
NadellaLokeshKumar <lokeshnadella99@gmail.com>
2020-02-26 12:21:34 +0530

    Assignment4

--- src/main/java/com/ito/car/repo/CarRepo.java
+++ src/main/java/com/ito/car/repo/CarRepo.java
@@ -8,11 +8,18 @@ import org.springframework.data.jpa.repository.Query;
 import com.ito.car.dto.Car;
 
 public interface CarRepo extends JpaRepository<Car, Integer>{
-	
+
+
 	@Query("from Car where type=:searchElement OR modelName=:searchElement")
 	public List<Car> search(String searchElement);
-	
+
+
+	/*
+	 * public List<Car> findByType(String Type); 
+	 * public List<Car>findByModelName(String modelName);    //we can use these also for searching(implicitly it only creates query).
+	 */
+
 	@Query("from Car where costPerDay<=:costPerDay")
 	public List<Car> searchByCost(double costPerDay);
-	
+
 }
